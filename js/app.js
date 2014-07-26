87217cd55081f8c153127367cc5170926e65a148
Hans <bmm79@nau.edu>
2014-07-26 10:38:40 -0700

    Build out app.js
    - added controllers for: index & about
    - product route
    - app.PRODUCTS (Json)
    
    finally added template scripts

--- js/app.js
+++ js/app.js
@@ -1,9 +1,44 @@
-App = Ember.Application.create({
-	 LOG_TRANSITIONS: true
+var App = Ember.Application.create({
+  LOG_TRANSITIONS: true
 });
-
 App.Router.map(function() {
+  this.route('credits', { path: '/thanks' });
   this.route('about');
-  this.route('credits');
+  this.resource('products');
+});
+App.IndexController = Ember.Controller.extend({
+  productsCount: 6,
+  logo: 'http://courseware.codeschool.com/ember/images/logo-small.png',
+  time: function() {
+    return (new Date()).toDateString();
+  }.property()
+});
+App.AboutController = Ember.Controller.extend({
+  contactName: 'Anostagia',
+  avatar: 'http://courseware.codeschool.com/ember/images/avatar.png',
+  open: function() {
+    return ((new Date()).getDay() === 0) ? "Closed" : "Open";
+  }.property()
 });
 
+App.ProductsRoute = Ember.Route.extend({
+  model: function() {
+    return App.PRODUCTS;
+  }
+});
+App.PRODUCTS = [
+  {
+    title: 'Flint',
+    price: 99,
+    description: 'Flint is a hard, sedimentary cryptocrystalline form of the mineral quartz, categorized as a variety of chert.',
+    isOnSale: true,
+    image: 'http://courseware.codeschool.com/ember/images/products/flint.png'
+  },
+  {
+    title: 'Kindling',
+    price: 249,
+    description: 'Easily combustible small sticks or twigs used for starting a fire.',
+    isOnSale: false,
+    image: 'http://courseware.codeschool.com/ember/images/products/kindling.png'
+  }
+];
